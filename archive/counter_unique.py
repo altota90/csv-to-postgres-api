ffe13e448575e217adc460629e234b9d5a1e57a0
alberto.tobarra <alberto.tobarra@gmail.com>
2026-07-21 16:08:40 +0100

    Move archive out of source control

--- archive/counter_unique.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/model_list.csv", low_memory=False)
-
-print(
-    df[["Generic Group", "Generic SubType"]]
-    .drop_duplicates()
-    .shape[0]
-)
--- archive/load_csv.py
+++
@@ -1,4 +0,0 @@
-from app.csv_loader import load_csv_to_postgres
-
-if __name__ == "__main__":
-    load_csv_to_postgres("data/datos.csv", "mi_tabla")
